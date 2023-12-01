95452ec5353f61e29a6d9e7c8a66fbd057ddf938
Mosè Giordano <mose@gnu.org>
2023-12-01 01:45:20 +0100

    This isn't needed

--- purgecss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: ['_site/**/*.html','_site/assets/js/main.js'],
-  css: ['_site/assets/css/main.css'],
-  output: '_site/assets/css/main.css',
-  fontFace: true,
-  safelist: {
-    deep: [/^leaflet/]
-  }
-}
